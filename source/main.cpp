cf105d3a641e428613fe91e11a133890a47208fd
Enrico Carraro s274930 <s274930@studenti.polito.it>
2020-08-22 11:02:03 +0200

    enabled again filename by argument

--- source/main.cpp
+++ source/main.cpp
@@ -63,13 +63,11 @@ int main(int argc, const char *argv[]) {
     FILE *fp;
 
     // 1 parameter of format .gra is required.
-    /*if (argc != 2) {
+    if (argc != 2) {
         cout << "Error: Missing parameter" << endl;
         return -1;
     }
-    string graname(argv[1]);*/
-
-    string graname("/home/lire/CLionProjects/sdp_pipelineSolution/v5000e50.gra");
+    string graname(argv[1]);
 
     if ((fp = fopen(graname.c_str(), "r")) == NULL) {
         cout << "Error: File doesn't exist." << endl;
